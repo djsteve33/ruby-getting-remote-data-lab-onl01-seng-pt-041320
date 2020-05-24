823c8ef095bd2edc82bff1aa3975f01d936b643e
Steve Womble <swomble@aol.com>
2020-05-24 19:20:09 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,9 +4,6 @@ require 'json'
 
 class GetRequester
   
-  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-  
-  
   def initialize(url)
     @url = url
     end
